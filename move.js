57f674a75ea0546d0b399a4363fd141dd5af7d5c
Chegrinets <katya.chegrinets@nixsolutions.com>
2014-09-26 13:20:23 +0300

    change server and module move

--- move.js
+++ move.js
@@ -5,19 +5,13 @@ module.exports = function move(client, message) {
         var parseString = message.slice(position, message.length),
             combinatons = JSON.parse(parseString),
             length = combinatons.length,
-            value = randomInt(0, length),
+            value = Math.floor(Math.random()*length),
             combination = combinatons.splice(value, 1)[0],
             data = [];
         data.push(combination);
         data.push(combinatons);
 
         var str = JSON.stringify(data);
-
-        console.log(str);
         client.write(str);
-
-        function randomInt(min, max) {
-            return min + ((max-min+1)*Math.random()^0);
-        }
     }
 };
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var net = require('net');
 var sockets = [];
+var socketsName = {
+    0 : 'Cross',
+    1: 'Zeroes'
+};
 var current = 0;
 var combinations = [
     [0, 0],
@@ -18,30 +22,43 @@ var server = net.createServer(function(socket) {
     console.log('connect');
     if(sockets.length < 2){
         sockets.push(socket);
-    } else{
+        console.log(sockets.length);
+    }
+    else{
         socket.destroy();
     }
     if(sockets.length == 2){
         sockets[0].write('move, ' + JSON.stringify(combinations))
+
     }
     socket.on('data', function (data) {
-        //while(combinations) {
-        var incomming = data.toString();
-        var separetor = incomming.indexOf(']');
-        var pos = incomming.slice(0, separetor + 1);
-        var position = JSON.parse(pos);
-        var comb = incomming.slice(separetor + 1, incomming.length);
-        combinations = JSON.parse(comb);
-        console.log(combinations);
-        if (position.length == 2) {
-            //
-            setPosition(current, field, comb);
-            //
-            current = current ? 0 : 1;
-            console.log('current:' + current);
-            console.log('pos:' + position);
-            console.log(combinations);
-            sockets[current].write('move, ' + JSON.stringify(combinations))
+        if(data.toString().indexOf('[') != -1) {
+            if(combinations.length > 0){
+                var currentData = JSON.parse(data.toString());
+                var position = currentData[0];
+                combinations = currentData[1];
+                if (position.length == 2) {
+                    setPosition(current, field, position);
+                    console.log('field');
+                    console.log(field);
+                    if(checkWinner(field)){
+                        for(var i = 0; i < sockets.length; i++){
+                            console.log('The winner is ' + socketsName[current]);
+                            sockets[i].write('The winner is ' + socketsName[current]);
+                            sockets[i].destroy();
+                        }
+                    } else if(!checkWinner(field) && combinations.length == 1) {
+                        for(var j = 0; j < sockets.length; j++){
+                            console.log('Draw');
+                            sockets[j].write('Draw!');
+                            sockets[j].destroy();
+                        }
+                    } else {
+                        current = current ? 0 : 1;
+                        sockets[current].write('move, ' + JSON.stringify(combinations))
+                    }
+                }
+            }
         }
     })
 }).listen(7777, function() {
@@ -52,4 +69,33 @@ function setPosition(current, field, comb) {
     var position1 = comb[0],
         position2 = comb[1];
     field[position1][position2] = current;
+}
+
+function checkWinner(field){
+    console.log('fields: ' + field[0][0], field[0][1]);
+    if(field[0][0] !== undefined && field[1][0] !== undefined && field[2][0] !== undefined && field[0][0] == field[1][0] && field[1][0] == field[2][0]){
+        return true;
+    }
+    if(field[0][1] !== undefined && field[1][1] !== undefined && field[2][1] !== undefined && field[0][1] == field[1][1] && field[1][1] == field[2][1]){
+        return true;
+    }
+    if(field[0][2] !== undefined && field[1][2] !== undefined && field[2][2] !== undefined && field[0][2] == field[1][2] && field[1][2] == field[2][2]){
+        return true;
+    }
+    if(field[0][0] !== undefined && field[1][1] !== undefined && field[2][2] !== undefined && field[0][0] == field[1][1] && field[1][1] == field[2][2]){
+        return true;
+    }
+    if(field[0][2] !== undefined && field[1][1] !== undefined && field[2][0] !== undefined && field[0][2] == field[1][1] && field[1][1] == field[2][0]){
+        return true;
+    }
+    else{
+        for(var i = 0; i < field.length; i ++){
+            for (var j = 0; j < field[i].length; j++){
+                if(field[i][j] === field[i][j + 1] && field[i][j + 1] == field[i][j + 2]){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
