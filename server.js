916fd511e14a39289ae3aa2b42d39fa6520655a0
Chegrinets <katya.chegrinets@nixsolutions.com>
2014-09-26 12:12:03 +0300

    write func setPosition

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var net = require('net');
-sockets = [];
-
+var sockets = [];
+var current = 0;
 var combinations = [
     [0, 0],
     [0, 1],
@@ -12,25 +12,44 @@ var combinations = [
     [2, 1],
     [2, 2]
 ];
-
+var field = [[],[],[]];
 
 var server = net.createServer(function(socket) {
     console.log('connect');
-    socket.write('Welcome');
-
-    if(sockets.length < 2) {
+    if(sockets.length < 2){
         sockets.push(socket);
-    } else {
+    } else{
         socket.destroy();
     }
-
-    if(sockets.length == 2) {
-        sockets[0].write('START GAME');
-        sockets[1].write('START GAME');
-        var combinationsV = JSON.stringify(combinations);
-        sockets[0].write('move ' + combinationsV);
+    if(sockets.length == 2){
+        sockets[0].write('move, ' + JSON.stringify(combinations))
     }
-
+    socket.on('data', function (data) {
+        //while(combinations) {
+        var incomming = data.toString();
+        var separetor = incomming.indexOf(']');
+        var pos = incomming.slice(0, separetor + 1);
+        var position = JSON.parse(pos);
+        var comb = incomming.slice(separetor + 1, incomming.length);
+        combinations = JSON.parse(comb);
+        console.log(combinations);
+        if (position.length == 2) {
+            //
+            setPosition(current, field, comb);
+            //
+            current = current ? 0 : 1;
+            console.log('current:' + current);
+            console.log('pos:' + position);
+            console.log(combinations);
+            sockets[current].write('move, ' + JSON.stringify(combinations))
+        }
+    })
 }).listen(7777, function() {
     console.log('Server is running!');
 });
+
+function setPosition(current, field, comb) {
+    var position1 = comb[0],
+        position2 = comb[1];
+    field[position1][position2] = current;
+}
