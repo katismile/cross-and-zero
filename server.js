1e1974fcc3773305e86efdfbf31f9de7387388c9
Chegrinets <katya.chegrinets@nixsolutions.com>
2014-09-29 18:19:40 +0300

    create module TikTakToe obj, change server

--- server.js
+++ server.js
@@ -17,36 +17,7 @@ var server = net.createServer(function(socket) {
         var message = data.toString();
 
         if(message.indexOf('[') !== -1){
-
-            var parseMessage = JSON.parse(message);
-            var id = parseMessage[0];
-
-            if( TikTakToe.games[id] &&  TikTakToe.games[id].combinations.length >=0){
-                var combination = parseMessage[1];
-                TikTakToe.games[id].combinations = parseMessage[2];
-                if(combination.length == 2){
-                    TikTakToe.setPosition( TikTakToe.games[id].current,  TikTakToe.games[id].field, combination);
-                    console.log( TikTakToe.games[id].field);
-                    if(TikTakToe.checkWinner( TikTakToe.games[id].field)){
-                        for(var k = 0; k <  TikTakToe.games[id].sockets.length; k++){
-                            console.log('The winner is ' +  TikTakToe.games[id].socketsName[ TikTakToe.games[id].current]);
-                            TikTakToe.games[id].sockets[k].write('The winner is ' +  TikTakToe.games[id].socketsName[ TikTakToe.games[id].current]);
-                        }
-                    }
-                    else if( !TikTakToe.checkWinner( TikTakToe.games[id].field) &&  TikTakToe.games[id].combinations.length == 0){
-                        for(var j = 0; j <  TikTakToe.games[id].sockets.length; j++){
-                            console.log('The game is finished, you both lost');
-                            TikTakToe.games[id].sockets[j].write('The game is finished, you both lost');
-                        }
-                    }
-                    else{
-                        TikTakToe.games[id].current =  TikTakToe.games[id].current ? 0 : 1;
-                        var secondMessage = [ TikTakToe.games[id].id,  TikTakToe.games[id].combinations];
-                        console.log('Current ' +  TikTakToe.games[id].current);
-                        TikTakToe.games[id].sockets[ TikTakToe.games[id].current].write(JSON.stringify(secondMessage));
-                    }
-                }
-            }
+            TikTakToe.move(message);
         }
     });
     socket.on('end', function() {
