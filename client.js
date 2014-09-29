c38c14e6ddeba838eb8df76e84813250770a6ae4
radistka <radistka.cat.net@gmail.com>
2014-09-29 17:38:52 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,52 @@
 var net = require('net');
 
-var client = new net.Socket();
+var argv = require('optimist').argv;
 
-client.connect(7777, function() {
-    //client.write('Hello, server');
-});
+if(argv.c) {
+    for(var j = 0; j < argv.c; j++) {
+        createSocket();
+    }
+} else {
+    createSocket();
+}
 
-client.on('data', function(data) {
-    console.log(data.toString());
-    if(data.toString().indexOf('winner') !== -1 || data.toString().indexOf('lost')){
-        //console.log(data.toString());
-    }
-    var message = data.toString();
-    if(message.indexOf('[') != -1) {
-        var parseString = JSON.parse(message),
-            combinatons = parseString[1],
-            id = parseString[0],
-            length = combinatons.length,
-            value = Math.floor(Math.random()*length),
-            combination = combinatons.splice(value, 1)[0],
-            data = [];
-        data.push(id);
-        data.push(combination);
-        data.push(combinatons);
-        console.log(data);
-        var str = JSON.stringify(data);
-        console.log(str);
-        client.write(str);
-    }
-});
+function createSocket() {
+    var client = new net.Socket({
+        writable: true
+    });
+
+    client.connect(7777, function() {
+        client.write('Hello Server!');
+    });
+
+    client.on('data', function(data) {
+        if(data.toString().indexOf('winner') !== -1 || data.toString().indexOf('lost')){
+            console.log(data.toString());
+        }
+        if(data.toString().indexOf('[') != -1) {
+            var message = data.toString();
+            var position = message.indexOf('[');
+            if(position != -1) {
+                setTimeout(function(){
+                    var parseString = message.slice(position, message.length);
+                    console.log("parseString " + parseString);
+                    var newArray = JSON.parse(parseString);
+                    var gameId = newArray[0];
+                    var combinatons = newArray[1];
+                    var length = combinatons.length;
+                    var value = Math.floor(Math.random()*length);
+                    var combination = combinatons.splice(value, 1)[0];
+                    var senddata = [];
+                    senddata.push(gameId);
+                    senddata.push(combination);
+                    senddata.push(combinatons);
+
+                    var str = JSON.stringify(senddata);
+                    client.write(str);
+                }, 2000)
+
+            }
+        }
+    });
+
+}
