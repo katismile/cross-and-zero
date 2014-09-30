b0295a6d1a60e703d73089d9da59656e5bbe678b
Chegrinets <katya.chegrinets@nixsolutions.com>
2014-09-30 15:38:30 +0300

    add module inquirer to the client

--- client.js
+++ client.js
@@ -1,31 +1,43 @@
-var net = require('net'),
-    move = require('./move'),
-    argv = require('optimist').argv;
+inquirer = require('inquirer');
 
-if(argv.c) {
-    for(var j = 0; j < argv.c; j++) {
-        createSocket();
-    }
-} else {
-    createSocket();
-}
+module.exports = function move(client, message) {
+    var position = message.indexOf('[');
+    if(position != -1) {
+        var parseString = message.slice(position, message.length);
+        console.log("parseString " + parseString);
+        newArray = JSON.parse(parseString);
+        gameId = newArray[0];
+        combinatons = newArray[1];
 
-function createSocket() {
-    var client = new net.Socket( {
-        allowHalfOpen: true,
-        readable: true,
-        writable: true
-    });
+        var choices = [];
 
-    client.connect(7777, function() {
-        client.write('Hello Server!');
-    });
-
-    client.on('data', function(data) {
-        if(data.toString().indexOf('[') != -1) {
-            setTimeout(function() {
-                move(client, data.toString());
-            }, 3000);
+        for(var i = 0; i < combinatons.length; i++) {
+            choices.push(JSON.stringify(combinatons[i]));
         }
-    });
-}
+
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "position",
+                message: "Please, choose position",
+                choices: choices
+            }
+        ], function( answer ) {
+            var combination = answer.position;
+
+            for(var j = 0; j < combinatons.length; j++) {
+                if( combinatons[j] + "" == JSON.parse(combination) ) {
+                    combinatons.splice(j,1);
+                }
+            }
+            data = [];
+            data.push(gameId);
+            data.push(JSON.parse(combination));
+            data.push(combinatons);
+
+            var str = JSON.stringify(data);
+            client.write(str);
+
+        });
+    }
+};
