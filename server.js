44b4c5df9a0c3b3b3910ae6ea210dab9da634c59
Iashyna Kateryna <katya.Iashyna@nixsolutions.com>
2014-10-03 10:35:51 +0300

    issue with multiple disconnect

--- server.js
+++ server.js
@@ -1,133 +1,82 @@
 var net = require('net');
-var checkWinner = require('./check_winner');
-var Game = require('./Game');
 var sockets = [];
-var games = [];
+var socketsPool = {};
+var redis = require('redis').createClient();
+var sub = require('redis').createClient();
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 var i = 0;
+var socketId = 0;
 
 var server = net.createServer(function(socket) {
-    console.log('connect');
     if (sockets.length < 2) {
         socket.gameId = i;
-        sockets.push(socket);
+        socket.socketId = socketId;
+        socketsPool[socketId] = socket;
+        sockets.push(socketId);
+        socketId++;
     }
     if (sockets.length == 2) {
-        start(games, i);
+        (async(function() {
+            var message = ['start game', [sockets, i]];
+            await(redis.lpush.bind(redis, 'tasks', JSON.stringify(message)));
+            sockets = [];
+            i++;
+        }))();
     }
     socket.on('data', function(data) {
         var message = data.toString();
         if (message.indexOf('[') !== -1) {
             var id = JSON.parse(message)[0];
-            if (games[id] && games[id].combinations.length >=0  && games[id].sockets.length === 2) {
-                var combination = JSON.parse(message)[1];
-                games[id].combinations = JSON.parse(message)[2];
-                if (combination.length == 2) {
-                    setPosition(games[id].current, games[id].field, combination);
-                    if (checkWinner(games[id].field)) {
-                        var winnerMessage = {
-                            setting: 'finish message',
-                            field: games[id].field,
-                            message: 'The winner is ' + games[id].socketsName[games[id].current]
-                        };
-                        messager(games[id].sockets, JSON.stringify(winnerMessage));
-                    }
-                    else if ( !checkWinner(games[id].field) && games[id].combinations.length == 0) {
-                        var lostMessage = {
-                            setting: 'finish message',
-                            field: games[id].field,
-                            message: 'The game is finished, both of you lost'
-                        };
-                        messager(games[id].sockets, JSON.stringify(lostMessage));
-                    }
-                    else {
-                        games[id].current = games[id].current == 1 ? 0 : 1;
-                        var secondMessage = {
-                            setting: 'choose position',
-                            id: games[id].id,
-                            combinations: games[id].combinations,
-                            field: games[id].field
-                        };
-                        games[id].sockets[games[id].current].write(JSON.stringify(secondMessage));
-
-                    }
-                }
-            }
+            var combination = JSON.parse(message)[1];
+            var combinations = JSON.parse(message)[2];
+            (async(function() {
+                var message = ['set position', [id, combination, combinations]];
+                await(redis.lpush.bind(redis, 'tasks', JSON.stringify(message)));
+            }))();
         }
     });
 
     socket.on('end', function() {
-        console.log('gameId on end: ' + socket.gameId);
-        if (sockets.indexOf(socket) != -1) {
-            console.log('in sockets');
+        if (sockets.indexOf(socket.socketId) != -1) {
             var index = sockets.indexOf(socket);
             sockets.splice(index, 1);
-            console.log(sockets);
         }
-
         var gameId = socket.gameId;
-        console.log(!!games[gameId]);
-        if (games[gameId]) {
-            var sock = socket == games[gameId].sockets[0] ? games[gameId].sockets[1] : games[gameId].sockets[0];
-            var message = {
-                setting: 'opponent exit',
-                message: 'You opponent has left, please wait for another player'
-            };
-//            console.log('i' + i);
-            sock.gameId = i;
-            sock.write(JSON.stringify(message));
-            games[gameId] = null;
-            sockets.push(sock);
-            if(sockets.length == 2) {
-                start();
-            }
-        }
-    })
+        var socketId = socket.socketId;
+        (async(function(){
+            var message = ['disconnect', [gameId, socketId]];
+            await(redis.lpush.bind(redis, 'tasks', JSON.stringify(message)));
+        }))();
+    });
 }).listen(7777, function() {
     console.log('Server is running!');
 });
 
-function setPosition(current, field, comb) {
-    var position1 = comb[0],
-        position2 = comb[1];
-
-    field[position1][position2] = current == 1 ? 0 : 'x';
-}
-function start() {
-    console.log('start');
-    var socketsName = {
-        0 : 'Cross',
-        1 : 'Zero'
-    };
-    var combinations = [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [1, 0],
-        [1, 1],
-        [1, 2],
-        [2, 0],
-        [2, 1],
-        [2, 2]
-    ];
-    var field = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
-    var couple = sockets;
-    sockets = [];
-
-    games[i] = new Game(i, combinations, field, socketsName, couple);
-    console.log('gameId on start: ' + games[i].id);
-    var message = {
-        setting: 'choose position',
-        id: games[i].id,
-        combinations: games[i].combinations,
-        field: games[i].field
-    };
-    games[i].sockets[games[i].current].write(JSON.stringify(message));
-    ++i;
-}
-function messager(sockets, message) {
-    for(var j = 0; j < sockets.length; j++){
-        sockets[j].write(message);
-        sockets[j].destroy();
+sub.subscribe('sockets commands');
+sub.subscribe('to stayed user');
+sub.on('message', function(channel, message){
+    if(channel == 'sockets commands'){
+        var socketId = JSON.parse(message)[0];
+        var messageToSocket = JSON.parse(message)[1];
+        console.log('all: '+Object.keys(socketsPool),'id: '+ socketId);
+        socketsPool[socketId].write(JSON.stringify(messageToSocket));
     }
-}
+    else if(channel == 'to stayed user'){
+        var sockId = JSON.parse(message).socketId;
+        socketsPool[sockId].gameId = i;
+        sockets.push(sockId);
+        if(sockets.length == 2){
+            (async(function() {
+                console.log(sockets, i);
+                var message = ['start game', [sockets, i]];
+                await(redis.lpush.bind(redis, 'tasks', JSON.stringify(message)));
+                sockets = [];
+                i++;
+            }))();
+        }
+    }
+
+});
+
 
---
+++ worker.js
@@ -0,0 +1,115 @@
+var redis = require('redis').createClient();
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var Game = require('./Game');
+var checkWinner = require('./check_winner');
+var games = [];
+var channel  = 'sockets commands';
+var stayed = 'to stayed user';
+var worker = async(function() {
+    while(true) {
+        var task = await(redis.brpop.bind(redis, 'tasks', 1000));
+        var parsed = JSON.parse(task[1]);
+        //console.log(parsed);
+        taskHandler[parsed[0]](parsed[1]);
+    }
+});
+worker();
+
+var taskHandler = {
+    'start game': function(args) {
+        var sockets = args[0];
+        var gameId = args[1];
+        start(games, gameId, sockets);
+    },
+    'set position': function(args) {
+        var id = args[0];
+        if(games[id] && games[id].combinations.length >= 0) {
+            var combination = args[1];
+            var combinations = args[2];
+            console.log('set position');
+            games[id].combinations = combinations;
+            setPosition(games[id].current, games[id].field, combination);
+            if (checkWinner(games[id].field)) {
+                var winnerMessage = {
+                    setting: 'finish message',
+                    field: games[id].field,
+                    message: 'The winner is ' + games[id].socketsName[games[id].current] + ', game: ' + id
+                };
+                redis.publish(channel, JSON.stringify([games[id].sockets[0], winnerMessage]));
+                redis.publish(channel, JSON.stringify([games[id].sockets[1], winnerMessage]));
+            }
+            else if (!checkWinner(games[id].field) && games[id].combinations.length == 0) {
+                var lostMessage = {
+                    setting: 'finish message',
+                    field: games[id].field,
+                    message: 'The game is finished, both of you lost'
+                };
+                redis.publish(channel, JSON.stringify([games[id].sockets[0], lostMessage]));
+                redis.publish(channel, JSON.stringify([games[id].sockets[1], lostMessage]));
+            }
+            else {
+                games[id].current = games[id].current == 1 ? 0 : 1;
+                var message = {
+                    setting: 'choose position',
+                    id: games[id].id,
+                    combinations: games[id].combinations,
+                    field: games[id].field
+                };
+                redis.publish(channel, JSON.stringify([games[id].sockets[games[id].current], message]));
+            }
+        }
+    },
+    'disconnect': function(args){
+        var gameId = args[0];
+        var socketId = args[1];
+        if(games[gameId]){
+            var stayedPlayer = socketId === games[gameId].sockets[0] ? games[gameId].sockets[1] : games[gameId].sockets[0];
+            var message = {
+                socketId: stayedPlayer
+            };
+            var messageForClient = {
+                setting: 'opponent exit',
+                message: 'You opponent has left, please wait for another player'
+            };
+            games[gameId] = null;
+            redis.publish(stayed, JSON.stringify(message));
+            redis.publish(channel, JSON.stringify([stayedPlayer,messageForClient]));
+        }
+    }
+};
+
+function start(games, i, sockets) {
+    console.log('start');
+    var socketsName = {
+        0: 'Cross',
+        1: 'Zero'
+    };
+    var combinations = [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [2, 0],
+        [2, 1],
+        [2, 2]
+    ];
+    var field = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
+
+    games[i] = new Game(i, combinations, field, socketsName, sockets);
+    var message = {
+        setting: 'choose position',
+        id: games[i].id,
+        combinations: games[i].combinations,
+        field: games[i].field
+    };
+    redis.publish(channel, JSON.stringify([games[i].sockets[games[i].current],message]));
+}
+function setPosition(current, field, comb) {
+    var position1 = comb[0],
+        position2 = comb[1];
+
+    field[position1][position2] = current == 1 ? 0 : 'x';
+}
