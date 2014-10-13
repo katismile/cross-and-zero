eadfe981432f7f02b6936c5a54618341b3d86da5
katismile <ekaterina_010@ukr.net>
2014-10-13 12:50:02 +0300

    change
    change

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var net = require('net'),
     argv = require('optimist').argv,
     tik_tak_toe = require('./tik_tak_toe'),
-    controller = require('./controller_for_client'),
+    controller = require('./controller'),
     inquirer = require('inquirer');
 
 if(argv.c) {
@@ -25,72 +25,24 @@ function createSocket() {
     });
 
     client.on('data', function(data) {
+        var options = {};
 
         if(typeof JSON.parse(data.toString()) === 'object') {
-            obj = JSON.parse(data.toString());
 
-            move(client, obj);
-        }
+            var action = JSON.parse(data.toString()).action;
 
-        if(typeof JSON.parse(data.toString()) === 'string') {
-            console.log(JSON.parse(data.toString()));
-
-            var action = JSON.parse(data.toString()),
-                options = {
-                    client: client
-                };
+            if(JSON.parse(data.toString()).data) {
+                options = JSON.parse(data.toString()).data;
+            }
+            options.client = client;
 
             if(typeof controller[action] === 'function') {
                 controller[action](options);
             }
         }
-    });
-}
 
-function move(client, message) {
-    var combinations = message.combinations,
-        field = message.field,
-        current  = message.current,
-        socketsNames = message.socketsName;
-
-        var setField =  message.isSmall ? "setSmallField" : "setBigField";
-
-        tik_tak_toe[setField](field, socketsNames);
-
-        var choices = [];
-
-        for(var i = 0; i < combinations.length; i++) {
-            choices.push(JSON.stringify(combinations[i]));
+        if(typeof JSON.parse(data.toString()) === 'string') {
+            console.log(JSON.parse(data.toString()));
         }
-
-        inquirer.prompt([
-            {
-                type: "list",
-                name: "position",
-                message: "Please, choose position",
-                choices: choices
-            }
-        ], function( answer ) {
-            var combination = answer.position;
-            field[JSON.parse(combination)[0]][JSON.parse(combination)[1]] = current;
-
-            tik_tak_toe[setField](field, socketsNames);
-
-            for(var j = 0; j < combinations.length; j++) {
-                if( combinations[j] + "" == JSON.parse(combination) ) {
-                    combinations.splice(j,1);
-                }
-            }
-
-            message['combination'] = JSON.parse(combination);
-            message['combinations'] = combinations;
-            message['field'] = field;
-
-
-            var obj = {
-                action: "check",
-                data: message
-            };
-            client.write(JSON.stringify(obj));
-        });
+    });
 }
